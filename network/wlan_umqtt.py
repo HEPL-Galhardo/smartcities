37a80bb65086fd40e36053abd6196ab416554d43
HEPL-Galhardo <124893862+HEPL-Galhardo@users.noreply.github.com>
2023-05-11 23:55:40 +0200

    Update wlan_umqtt.py

--- network/wlan_umqtt.py
+++ network/wlan_umqtt.py
@@ -15,8 +15,8 @@ def sub_cb(topic, msg):
     
 #---------------------------------------Wifi Connection-------------------------------------------
 #Wifi connection: setting the identifier and the password to the wifi connection
-SSID="Android Galhardo"				#Kot: SSID="Proximus-Home-50F8"  PASSWORD="wz92j9eaey3pc"
-PASSWORD="Galhardo1996"				#HEPL: SSID="electroProjectWifi"  PASSWORD="M13#MRSE"
+SSID=""
+PASSWORD=""
 wlan = network.WLAN(network.STA_IF)
 #Enabeling the wlan
 wlan.active(True)
@@ -46,26 +46,20 @@ print("IP Adresse informations: ",wlan.ifconfig())
 #---------------------------------------MQTT Server Connection-------------------------------------------
 #Connection to the server MQTT
 print('Authetification for the connecting to MQTT Broker')
-monClientMqtt = MQTTClient(client_id="PicoIoT", #Tritan: picow			#Notre: PicoIoT , PicoIoT1
-                    server=b"3715debebfa546d29fddbf13ddd328f1.s2.eu.hivemq.cloud",   
+monClientMqtt = MQTTClient(client_id="PicoIoT",
+                    server=b"",   
                     port=8883, 
                     user= "PicoIoT", 
-                    password="PicoIoT1234", #Tritan: Azertyuiop1			#Notre: PicoIoT1234
+                    password="PicoIoT1234",
                     keepalive=7200,
                     ssl=True,
-                    ssl_params={'server_hostname':'3715debebfa546d29fddbf13ddd328f1.s2.eu.hivemq.cloud'})
-#Tritan: c0deea5d7e584e5fadc9488cba7cb0bf.s2.eu.hivemq.cloud			#Notre: 3715debebfa546d29fddbf13ddd328f1.s2.eu.hivemq.cloud
+                    ssl_params={'server_hostname':''})
 sleep(1)
-
 #Connection to the server MQTT
 print("Connection to the MQTT server")
 monClientMqtt.connect()
 print("Fonction Callback MQTT")
 monClientMqtt.set_callback(sub_cb)
-
-#----------Publishing--------------
-#print("Envoi d'un message au serveur MQTT")
-#monClientMqtt.publish(b"Topic",b"HellodemonPico2W!")
 
 #----------Subscrition--------------
 print("Subscrition to a topic in the MQTT server")
@@ -73,12 +67,10 @@ monClientMqtt.subscribe("Test")
 
 while True:
     
-    # Non-blocking wait for message
+    # Non-blocking waiting for message
     print("Checking for message from the MQTT server")
     monClientMqtt.check_msg()
-       
-    # Then need to sleep to avoid 100% CPU usage (in a real
-    # app other useful actions would be performed instead)
     sleep(3)
+    #----------Publishing--------------
     print("Publishing a message to the MQTT server")
     monClientMqtt.publish(b"Topic",b"Message")
